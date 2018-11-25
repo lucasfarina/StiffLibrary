996f461c292d3ab065a2bc43ec885eb66377fd37
lucasfarina <lugifarina@gmail.com>
2018-11-24 22:16:05 -0200

    Added FName magic!
    FName is a magic structure that serves as an ID.
    
    It enables the simpleness of understanding contexts that strings provide, and the efficiency that integers provide.
    
    REMEMBER THE MAP IS FILLED RUNTIME, SO THE SAME STRING MAY HAVE DIFFERENT ID'S ON DIFFERENT PROCESSES AND/OR MACHINES, AND WILL NOT BE CONSIDERED EQUAL WHEN COMPARING.
    
    FOR PERSISTANCE: SAVE IT AS A STRING AND RECONSTRUCT IT AS FNAME WHEN READING.
    
    FOR MESSAGING: CONVERT IT TO A STRING, SEND IT, THEN RECONSTRUCT IT AS FNAME ON THE OTHER SIDE.

---
+++ StiffLibrary/FName.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StiffLibrary
+{
+    public struct FName
+    {
+        private const int _initialMapChunk = 50;
+        private static int _mapChunk = _initialMapChunk;
+        private static string[] _nameMap = new string[_mapChunk];
+        private static int _mapNextId = 1;
+        public static readonly FName None = new FName();
+
+        private int _id;
+        private string _name;
+
+        public FName(string name = null)
+        {
+            string safeName = name.ToUpper();
+            if(safeName != null || safeName == "None")
+            {
+                _name = safeName;
+                int myId = Array.FindIndex<string>(_nameMap, x => x == safeName);
+                if (myId >= 0)
+                {
+                    _id = myId;
+                }
+                else
+                {
+                    if ((_mapNextId) >= _nameMap.Length)
+                    {
+                        _mapChunk *= 2;
+                        int newSize = _nameMap.Length + _mapChunk;
+                        Array.Resize<string>(ref _nameMap, newSize);
+                    }
+                    _nameMap[_mapNextId] = safeName;
+                    _id = _mapNextId;
+                    _mapNextId++;
+                }
+            }
+            else
+            {
+                _name = null;
+                _id = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            if(_id == 0)
+            {
+                return "NONE";
+            }
+            return _nameMap[_id];
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj.GetType() == this.GetType())
+            {
+                FName other = (FName)obj;
+                return other._id == _id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return _id;
+        }
+    }
+}
