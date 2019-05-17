35566f99ad1d171d904b48e922ed90e5f5da1bff
Lucas Farina <lucas@garage227studios.com>
2019-05-17 01:12:18 -0300

    Set simple Pile class to public

---
+++ StiffLibrary/Pile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StiffLibrary
+{
+    public class Pile<T>
+    {
+        public Pile()
+        {
+            items = new List<T>();
+        }
+
+        private List<T> items;
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            T answer = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+            return answer;
+        }
+
+        public int PileSize()
+        {
+            return items.Count;
+        }
+
+        public T GetTop()
+        {
+            return items[items.Count - 1];
+        }
+    }
+}
--- StiffLibrary/XML/XMLManager.cs
+++ StiffLibrary/XML/XMLManager.cs
@@ -348,36 +348,4 @@ namespace StiffLibrary.XML
             }
         }
     }
-
-    internal class Pile<T>
-    {
-        public Pile()
-        {
-            items = new List<T>();
-        }
-
-        private List<T> items;
-
-        public void Push(T item)
-        {
-            items.Add(item);
-        }
-
-        public T Pop()
-        {
-            T answer = items[items.Count - 1];
-            items.RemoveAt(items.Count - 1);
-            return answer;
-        }
-
-        public int PileSize()
-        {
-            return items.Count;
-        }
-
-        public T GetTop()
-        {
-            return items[items.Count - 1];
-        }
-    }
 }
